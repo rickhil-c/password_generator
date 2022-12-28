634017a0ce7b8cdb8469460c9e6b3e96c91fded0
rickhil-c <R6ZsU@GT6zWX3pq>
2022-12-29 09:43:38 +1200

    fixed linter warning

--- password_generator.py
+++ password_generator.py
@@ -7,6 +7,7 @@ initial_response = input("Do you want to generate some passwords?")
 yes = True
 no = False
 
+
 def get_password(length):
     # r = "".join(random.choice(chars) for i in range(int(length)))
     r = ""
@@ -14,6 +15,7 @@ def get_password(length):
         c = random.choice(chars)
         r = r + c
     return r
+
 
 if initial_response:
     length = input("How long do you want your password to be?")
